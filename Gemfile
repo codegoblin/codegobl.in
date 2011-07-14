20136d294f4740eaccf4faafed1a2647e81071ee
Cody Marshall Johnston <cody@codegobl.in>
2011-07-13 23:04:00 -0700

    working on getting a twitter feed going

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem "pg", "~> 0.11.0"
 gem "passenger", "~> 3.0.7"
 gem "capistrano", "~> 2.6.0"
 gem "sass", "~> 3.1.2"
+gem "twitter", "~> 1.6.0"
 
 group :development do
   gem "ruby-debug19", "~> 0.11.6", :require => 'ruby-debug'
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = CONFIG['twitter']['consumer_key']
+  config.consumer_secret = CONFIG['twitter']['consumer_secret']
+  config.oauth_token = CONFIG['twitter']['oauth_token']
+  config.oauth_token_secret = CONFIG['twitter']['oauth_token_secret']
+end
