09012795252e86fd01b1deecd8938558c8159028
Cody Marshall Johnston <cody@codegobl.in>
2011-07-17 18:32:12 -0700

    damnnnitt rraiillss

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CodeGoblinWeb::Application.config.secret_token = 'e9c5a9ac3a260290d0b421b5c6f39a967f32f2ed3618ac8b6604f2ed534a44996576958a9c3f7bbb3f18792f16c97d52e02ab1d69a80bb41cb52743cd574495d'
+CodeGoblinWeb::Application.config.secret_token = CONFIG['secret_token']
