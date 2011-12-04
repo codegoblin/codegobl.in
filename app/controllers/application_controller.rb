f1df969469d5c124c57e9762c4d180469a552480
Cody Johnston <cody@codegobl.in>
2011-12-03 20:09:11 -0800

    change namespaces; delete entries_controller; refactor or two;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,5 @@ class ApplicationController < ActionController::Base
   ###########
   # Methods #
   ###########
+
 end
--- app/controllers/public/blog_controller.rb
+++ app/controllers/public/blog_controller.rb
@@ -1,5 +1,5 @@
-class BlogController < ApplicationController
-  def index
+class Public::BlogController < ApplicationController
+  def show
     @twitter_statuses = TWITTER_CLIENT.user_timeline[0..4] # TODO: Substitute range with constant
   end
 end
