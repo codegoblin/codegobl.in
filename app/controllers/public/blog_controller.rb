67870a693e9e431a316b5e3a14bfa32ba0db934d
Cody Johnston <cody@codegobl.in>
2011-12-05 02:48:15 -0800

    refactor config

--- app/controllers/public/blog_controller.rb
+++ app/controllers/public/blog_controller.rb
@@ -1,8 +1,8 @@
 class Public::BlogController < ApplicationController
   def show
-    @twitter_entries = Twitter.home_timeline[0...Codegoblin::NUM_TWEETS_SHOWN]
+    @twitter_entries = Twitter.user_timeline[0...Codegoblin::Config::NUM_TWEETS_SHOWN]
     @github_entries = Feedzirra::Feed.
-                      fetch_and_parse(Codegoblin::GITHUB_FEED_ADDRESS).
-                      entries[0...Codegoblin::NUM_GITHUB_ENTRIES_SHOWN]
+                      fetch_and_parse(Codegoblin::Config::GITHUB_FEED_ADDRESS).
+                      entries[0...Codegoblin::Config::NUM_GITHUB_ENTRIES_SHOWN]
   end
 end
--- config/initializers/codegoblin.rb
+++ config/initializers/codegoblin.rb
@@ -1,10 +1,11 @@
+
 module Codegoblin
-  # This is a config module for general app settings
+  class Config
+    # TWITTER
+    NUM_TWEETS_SHOWN = 5
 
-  # TWITTER
-  NUM_TWEETS_SHOWN = 5
-
-  # GITHUB APP
-  NUM_GITHUB_ENTRIES_SHOWN = 5
-  GITHUB_FEED_ADDRESS = "https://github.com/codegoblin.atom"
+    # GITHUB APP
+    NUM_GITHUB_ENTRIES_SHOWN = 5
+    GITHUB_FEED_ADDRESS = "https://github.com/codegoblin.atom"
+  end
 end
