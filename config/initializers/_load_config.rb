714f0dd3f494d4d05e22d7b4954fcaf87e11263e
Cody Johnston <cody@codegobl.in>
2011-12-05 02:25:39 -0800

    third times a charm?

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -1,10 +1,12 @@
-config_filename = (Rails.env.production? ? "config.example.yml" : "config.yml")
 module Codegoblin 
   class Config
     @@config = nil
 
     def self.[](key)
-      (@@config ||= HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('/config/', config_filename).to_s))[Rails.env])[key]
+      (@@config ||= HashWithIndifferentAccess.new(
+                      YAML.load_file(
+                        Rails.root.join('/config/', (Rails.env.production? ? "config.example.yml" : "config.yml")).to_s
+                      ))[Rails.env])[key]
     end
   end
 end
