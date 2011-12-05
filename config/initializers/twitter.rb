face8fbd604beeb2b9ba81588601edfe8fd012c8
Cody Johnston <cody@codegobl.in>
2011-12-05 01:46:50 -0800

    heroku env/config fix

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,6 @@
 Twitter.configure do |config|
-  twitter_config = Codegoblin::Config[:twitter]
-  config.consumer_key = ENV['TWITTER_CONSUMER_KEY'] || twitter_config['consumer_key']
-  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] || twitter_config['consumer_secret']
-  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN'] || twitter_config['oauth_token']
-  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET'] || twitter_config['oauth_token_secret']
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY'] || Codegoblin::Config[:twitter]['consumer_key']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] || Codegoblin::Config[:twitter]['consumer_secret']
+  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN'] || Codegoblin::Config[:twitter]['oauth_token']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET'] || Codegoblin::Config[:twitter]['oauth_token_secret']
 end
